1db7edd1719bc70d1bb565a8de86a9ccf3ea4e1e
@EthanStyle <brownev19@gcc.edu>
2023-03-24 23:02:54 -0400

    Update HumanGuessesGameTest.java
    Added dep inj comment

--- test/HumanGuessesGameTest.java
+++ test/HumanGuessesGameTest.java
@@ -13,6 +13,7 @@ public class HumanGuessesGameTest {
 
     @Test
     public void HumanGuessesGameTest() {
+        //using dependency injection
         MockHumanGuessesGame g = new MockHumanGuessesGame(1);
         assertEquals(1, g.getTarget());
         g = new MockHumanGuessesGame(500);
@@ -25,12 +26,14 @@ public class HumanGuessesGameTest {
 
     @Test
     public void MakeGuessTest_Correct() {
+        //using dependency injection
         assertEquals(GuessResult.CORRECT, game.makeGuess(250));
         assert game.isDone();
     }
 
     @Test
     public void MakeGuessTest_Low() {
+        //using dependency injection
         assertEquals(GuessResult.LOW, game.makeGuess(1));
         assertEquals(GuessResult.LOW, game.makeGuess(5));
         assertEquals(GuessResult.LOW, game.makeGuess(249));
@@ -38,6 +41,7 @@ public class HumanGuessesGameTest {
 
     @Test
     public void MakeGuessTest_High() {
+        //using dependency injection
         assertEquals(GuessResult.HIGH, game.makeGuess(251));
         assertEquals(GuessResult.HIGH, game.makeGuess(900));
         assertEquals(GuessResult.HIGH, game.makeGuess(1000));
@@ -45,6 +49,7 @@ public class HumanGuessesGameTest {
 
     @Test
     public void MakeGuessTest_IncrementsNumGames() {
+        //using dependency injection
         game.makeGuess(250);
         assertEquals(1, game.getNumGuesses());
         game.makeGuess(250);
