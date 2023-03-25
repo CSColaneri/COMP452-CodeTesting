085ed0c08eb503f3744c6cfe130db376b48f7146
@EthanStyle <brownev19@gcc.edu>
2023-03-24 23:03:06 -0400

    Update ComputerGuessesGameTest.java
    Extra tests

--- test/ComputerGuessesGameTest.java
+++ test/ComputerGuessesGameTest.java
@@ -13,19 +13,36 @@ class ComputerGuessesGameTest {
     }
 
     @Test
-    void makeNextGuessTest() {
-        // TODO: should this be 500 or 501?
+    void makeFirstGuessTest() {
         assertEquals(500, game.makeNextGuess(true));
     }
 
+
     @Test
-    void updateBoundsTest() {
-        // TODO: Depending on above answer, change this accordingly. (+1)
+    void updateBoundsTest_tooLow() {
         game.makeNextGuess(true);
         assertEquals(1, game.getLowerBound());
         assertEquals(1000, game.getUpperBound());
         game.makeNextGuess(true);
         assertEquals(501, game.getLowerBound());
         assertEquals(1000, game.getUpperBound());
+        for(int i = 0; i < 9; ++i) {
+            game.makeNextGuess(true);
+        }
+        assertEquals(1000, game.getLastGuess());
+    }
+
+    @Test
+    void updateBoundsTest_tooHigh() {
+        game.makeNextGuess(true);
+        assertEquals(1, game.getLowerBound());
+        assertEquals(1000, game.getUpperBound());
+        game.makeNextGuess(false);
+        assertEquals(1, game.getLowerBound());
+        assertEquals(500, game.getUpperBound());
+        for(int i = 0; i < 9; ++i) {
+            game.makeNextGuess(false);
+        }
+        assertEquals(1, game.getLastGuess());
     }
 }
